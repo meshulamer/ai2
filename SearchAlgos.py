7f92ada6f31799a10d63d4de37f991c4efe9f279
eilon299 <57979428+eilon299@users.noreply.github.com>
2020-12-19 11:16:22 +0200

    eilon debugged minimax

--- SearchAlgos.py
+++ SearchAlgos.py
@@ -44,11 +44,13 @@ class MiniMax(SearchAlgos):
             depth += 1
             result = self.search_helper(state, depth, 1)
 
+        print("after itr res is{}".format(result))
         passed_time = time.time() - start_time
         remaining_time = time_limit - passed_time
         iteration_start_time = iteration_finish_time = iteration_delta = 0
 
-        for move in self.succ():
+        for move in self.succ(state, 1):
+            print("trying son after itr")
             iteration_delta = iteration_finish_time - iteration_start_time
             iteration_start_time = time.time()
             remaining_time -= iteration_delta
@@ -60,30 +62,31 @@ class MiniMax(SearchAlgos):
                     result[0] = move
                     result[1] = son_result[1]
             iteration_finish_time = time.time()
+            print("after son res is{}".format(result))
 
         return result[0]
 
     def search_helper(self, state, depth, maximizing_player):
-        if maximizing_player == 0:
+        if maximizing_player == 1:
             best = [None, -math.inf]
         else:
             best = [None, math.inf]
         if depth == 0:
-            score = state.h()
-            return [None, score]
+            return [None, self.utility(state)]
 
-        move = score = None
-        for move in self.succ():
-            self.perform_move(state, maximizing_player, move)
-            [move, score] = self.search_helper(state, depth - 1, 1 - maximizing_player)
-            self.undo_move(state, maximizing_player, move)
-
-        if maximizing_player == 1:
-            if score > best[1]:
-                best = [move, score]
-        else:
-            if score < best[1]:
-                best = [move, score]
+        legal_moves = self.succ(state, maximizing_player)
+        if legal_moves == []:
+            return [None, self.utility(state)]
+        for legal_move in legal_moves:
+            self.perform_move(state, maximizing_player, legal_move)
+            res = self.search_helper(state, depth - 1, 1 - maximizing_player)
+            self.undo_move(state, maximizing_player, legal_move)
+            if maximizing_player == 1:
+                if res[1] > best[1]:
+                    best = [legal_move, res[1]]
+            else:
+                if res[1] < best[1]:
+                    best = [legal_move, res[1]]
         return best
 
 
