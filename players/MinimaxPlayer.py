2ab4e9638a5d90488dcbeec1066679cd880ccf69
eilon299 <57979428+eilon299@users.noreply.github.com>
2020-12-19 11:16:22 +0200

    eilon debugged minimax

--- players/MinimaxPlayer.py
+++ players/MinimaxPlayer.py
@@ -11,9 +11,9 @@ import SearchAlgos
 # TODO: you can import more modules, if needed
 class MinMaxState:
     def __init__(self, board, penalty_score):
+        self.current_board = board
         self.fruit_turn_timer = math.ceil(2*min(len(self.current_board), len(self.current_board[0])))
         self.penalty_score = penalty_score
-        self.current_board = board
         self.available_fruits = {}
         self.removed_fruits = {}
         self.scores = [0, 0]
@@ -21,14 +21,14 @@ class MinMaxState:
         self.locations = [None, None]
         for i in range(0, len(board)):
             for j in range(0, len(board[i])):
-                if board[i, j] == 0:
+                if board[i][j] == 0:
                     continue
-                if board[i, j] == 1:
+                if board[i][j] == 1:
                     self.locations[0] = (i, j)
-                if board[i, j] == 2:
+                if board[i][j] == 2:
                     self.locations[1] = (i, j)
-                if board[i, j] > 2:
-                    self.available_fruits[(i, j)] = board[i, j]
+                if board[i][j] > 2:
+                    self.available_fruits[(i, j)] = board[i][j]
         assert (self.locations[0] is not None and self.locations[1] is not None)
 
 
@@ -60,6 +60,7 @@ class Player(AbstractPlayer):
             - direction: tuple, specifing the Player's movement, chosen from self.directions
         """
         move = self.searchAlgo.search(self.currentState, time_limit)
+        assert(move!= None)
         preformMove(self.currentStateInPlay, 1, move)
         return move
 
@@ -70,14 +71,14 @@ class Player(AbstractPlayer):
             - pos: tuple, the new position of the rival.
         No output is expected
         """
-        if self.currentStateInPlay.current_board[pos[0], pos[1]] > 2:
-            fruit_value = self.currentStateInPlay.current_board[pos[0], pos[1]]
+        if self.currentStateInPlay.current_board[pos[0]][pos[1]] > 2:
+            fruit_value = self.currentStateInPlay.current_board[pos[0]][pos[1]]
             self.currentStateInPlay.scores[1] += fruit_value
             self.currentStateInPlay.removed_fruits[pos] = fruit_value
             del self.currentStateInPlay.available_fruits[pos]
-        self.currentStateInPlay.current_board[pos[0], pos[1]] = 2
+        self.currentStateInPlay.current_board[pos[0]][pos[1]] = 2
         self.currentStateInPlay.current_board[
-            self.currentStateInPlay.locations[1][0], self.currentStateInPlay.locations[1][1]] = -1
+            self.currentStateInPlay.locations[1][0]][self.currentStateInPlay.locations[1][1]] = -1
         self.currentStateInPlay.locations[1] = pos
         self.currentStateInPlay.turns_played += 1
         self.currentState = deepcopy(self.currentStateInPlay)
@@ -106,27 +107,30 @@ def succ(state, player):  # (Up/Down , Right/Left)
     for dir in get_directions():
         newloc = (p_loc[0] + dir[0], p_loc[1] + dir[1])
         if 0 <= newloc[0] < len(state.current_board):
-            if 0 <= newloc[1] < len(state.board[0]):
-                if state.current_board[newloc[0], newloc[1]] == 0 or state.current_board[newloc[0], newloc[1]] > 2:
-                    moves.append(newloc)
+            if 0 <= newloc[1] < len(state.current_board[0]):
+                if state.current_board[newloc[0]][newloc[1]] == 0 or state.current_board[newloc[0]][newloc[1]] > 2:
+                    moves.append(dir)
     return moves
 
-
-def heuristic(state, depth):
-    pass
+def heuristic(state):
+    sum_scores = (state.scores[0]+state.scores[1])
+    if (sum_scores == 0):
+        return 0
+    else:
+        return state.scores[0]/sum_scores
 
 
 def preformMove(state, player, move):
     assert (type(state) == MinMaxState)
     oldloc = state.locations[1-player]
     newloc = (oldloc[0] + move[0], oldloc[1] + move[1])
-    assert(state.current_board[newloc[0], newloc[1]] == 0 or state.current_board[newloc[0], newloc[1]] > 2)
-    if state.current_board[newloc[0], newloc[1]] > 0 and state.turns_counter <= state.fruit_turn_timer:
-        state.scores[1-player] += state.current_board[newloc[0], newloc[1]]
-        state.removed_fruits[newloc] = state.available_Fruits[newloc]
-        del state.available_Fruits[newloc]
-    state.current_board[newloc[0], newloc[1]] = 2 - player
-    state.current_board[oldloc[0], oldloc[1]] = -1
+    assert(state.current_board[newloc[0]][newloc[1]] == 0 or state.current_board[newloc[0]][newloc[1]] > 2)
+    if state.current_board[newloc[0]][newloc[1]] > 0 and state.turns_played <= state.fruit_turn_timer:
+        state.scores[1-player] += state.current_board[newloc[0]][newloc[1]]
+        state.removed_fruits[newloc] = state.available_fruits[newloc]
+        del state.available_fruits[newloc]
+    state.current_board[newloc[0]][newloc[1]] = 2 - player
+    state.current_board[oldloc[0]][oldloc[1]] = -1
     state.locations[1-player] = newloc
     state.turns_played += 1
 
@@ -139,12 +143,12 @@ def undoMove(state, player, move):
     if currloc in state.removed_fruits:
         state.available_fruits[currloc] = state.removed_fruits[currloc]
         del state.removed_fruits[currloc]
-        if state.turns_counter <= state.fruit_turn_timer:
+        if state.turns_played <= state.fruit_turn_timer:
             state.scores[1-player] -= state.available_fruits[currloc]
-        state.current_board[currloc[0], currloc[1]] = state.available_fruits[currloc]
+        state.current_board[currloc[0]][currloc[1]] = state.available_fruits[currloc]
     else:
-        state.current_board[currloc[0], currloc[1]] = 0
-    state.current_board[prevloc[0], prevloc[1]] = 2 - player
+        state.current_board[currloc[0]][currloc[1]] = 0
+    state.current_board[prevloc[0]][prevloc[1]] = 2 - player
     state.locations[1 - player] = prevloc
 
 
